dc9666bd1bbfcd620f9fe79ebb5255e5c66012be
zhangxinqiang <1933581361@qq.com>
2021-03-18 23:43:46 +0800

    二叉树题目 652. 寻找重复的子树 中等

---
+++ src/main/java/com/fd/algorithmlearn/tree/FindDuplicateSubtreesAlgo.java
@@ -0,0 +1,50 @@
+package com.fd.algorithmlearn.tree;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 652. 寻找重复的子树 中等
+ * https://leetcode-cn.com/problems/find-duplicate-subtrees/
+ *
+ * @author : zxq
+ * @date-time : 2021/3/18 23:41
+ */
+public class FindDuplicateSubtreesAlgo {
+
+    HashMap<String, Integer> memo = new HashMap<>();
+
+    LinkedList<TreeNode> res = new LinkedList<>();
+
+    /**
+     * 主函数
+     */
+    List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        traverse(root);
+        return res;
+    }
+
+    String traverse(TreeNode root) {
+        // 对于空节点，可以用一个特殊字符表示
+        if (root == null) {
+            return "#";
+        }
+        // 将左右字数序列化为字符串
+        String left = traverse(root.left);
+        String right = traverse(root.right);
+        /* 后序遍历代码位置 */
+        // 左右子树加上自己，就是以自己为根的二叉树序列化结果
+        String subTree = left + "," + right + "," + root.val;
+
+        int freq = memo.getOrDefault(subTree, 0);
+        // 多次重复也只会被加入结果集一次
+        if (freq == 1) {
+            res.add(root);
+        }
+        // 给子树对应的出现次数加一
+        memo.put(subTree, freq + 1);
+        return subTree;
+    }
+
+}
--- src/main/java/com/fd/algorithmlearn/tree/TreeUtil.java
+++ src/main/java/com/fd/algorithmlearn/tree/TreeUtil.java
@@ -1,5 +1,10 @@
 package com.fd.algorithmlearn.tree;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author ：zxq
  * @date ：Created in 2021/3/15 17:45
@@ -21,5 +26,19 @@ public class TreeUtil {
         print(root.right);
     }
 
+    /**
+     * 二叉树的节点数
+     */
+    int count(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // 先算出左右子树有多少节点
+        int left = count(root.left);
+        int right = count(root.right);
+        /* 后序遍历代码位置 */
+        // 加上自己，就是整棵二叉树的节点数
+        return 1 + left + right;
+    }
 
 }
