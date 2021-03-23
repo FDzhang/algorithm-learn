c212d34725f226dcbf8c4c80df6854e0f2d33b82
FDzhang <1933581361@qq.com>
2021-03-23 14:34:25 +0800

    二叉树 230. 二叉搜索树中第K小的元素

---
+++ src/main/java/com/fd/algorithmlearn/tree/KthSmallestAlgo.java
@@ -0,0 +1,39 @@
+package com.fd.algorithmlearn.tree;
+
+/**
+ * 230. 二叉搜索树中第K小的元素 中等
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+ *
+ * @author ：zxq
+ * @date ：Created in 2021/3/23 14:25
+ */
+
+public class KthSmallestAlgo {
+    public int kthSmallest(TreeNode root, int k) {
+        // 利用 BST 的中序遍历特性
+        kth(root, k);
+        return val;
+    }
+
+    // 记录当前元素的排名
+    private int rank = 0;
+    // 记录结果
+    private int val = 0;
+
+    public void kth(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+
+        kth(root.left, k);
+        /* 中序遍历代码位置 */
+        rank++;
+        if (rank == k) {
+            // 找到第 k 小的元素
+            val = root.val;
+            return;
+        }
+
+        kth(root.right, k);
+    }
+}
