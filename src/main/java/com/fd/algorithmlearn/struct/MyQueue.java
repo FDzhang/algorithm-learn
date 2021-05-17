578dfec7ecc1ea9ab6ace849f8146b3e206dda8c
zhangxinqiang <1933581361@qq.com>
2021-05-17 19:31:57 +0800

    数据结构 232. 用栈实现队列

---
+++ src/main/java/com/fd/algorithmlearn/struct/MyQueue.java
@@ -0,0 +1,57 @@
+package com.fd.algorithmlearn.struct;
+
+import java.util.Stack;
+
+/**
+ * 232. 用栈实现队列
+ * https://leetcode-cn.com/problems/implement-queue-using-stacks/
+ *
+ * @author zhangxinqiang
+ * @create 2021/5/17 16:25
+ */
+public class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        peek();
+        return stack2.pop();
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
