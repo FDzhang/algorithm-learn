fbc20d5ad0cbd2994f26134fada9d239568b3246
zhangxinqiang <1933581361@qq.com>
2021-05-27 10:27:00 +0800

    leetcode 初級算法題

--- src/main/java/com/fd/algorithmlearn/Temp.java
+++ src/main/java/com/fd/algorithmlearn/Temp.java
@@ -180,6 +180,14 @@ public class Temp {
         return true;
     }
 
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+    }
+
     public static void main(String[] args) {
 //        System.out.println(1 << 1);
 //        System.out.println(1 << 2);
