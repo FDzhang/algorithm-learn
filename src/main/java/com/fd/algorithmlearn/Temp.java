3bedbfc1c0f14711e897f3b6bc48f5b3835b3c45
zhangxinqiang <1933581361@qq.com>
2021-06-22 18:36:13 +0800

    leetcode 初级算法 验证二叉搜索树

--- src/main/java/com/fd/algorithmlearn/Temp.java
+++ src/main/java/com/fd/algorithmlearn/Temp.java
@@ -626,6 +626,40 @@ public class Temp {
         return Math.max(left, right) + 1;
     }
 
+    /**
+     * 验证二叉搜索树
+     * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+     * <p>
+     * 假设一个二叉搜索树具有如下特征：
+     * <p>
+     * 节点的左子树只包含小于当前节点的数。
+     * 节点的右子树只包含大于当前节点的数。
+     * 所有左子树和右子树自身必须也是二叉搜索树。
+     *
+     * 思路：
+     * 1. root节点的值 要大于左节点，要小于右节点
+     * 2. root节点的值 是左子树的最大值，是右子树的最小值
+     */
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+    /* 限定以 root 为根的子树节点必须满足 max.val > root.val > min.val */
+    public boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        // base case
+        if (root == null) {
+            return true;
+        }
+        // 若 root.val 不符合 max 和 min 的限制，说明不是合法 BST
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+        if (max != null && root.val >= max.val) {
+            return false;
+        }
+        // 限定左子树的最大值是 root.val，右子树的最小值是 root.val
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
     public static void main(String[] args) {
         ListNode head = new ListNode();
         ListNode n1 = new ListNode();
