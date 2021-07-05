2c94d5bec7827de489f71a4dba2722f4758b151e
zhangxinqiang <1933581361@qq.com>
2021-07-05 10:56:36 +0800

    leetcode 初级算法 第一个错误的版本

--- src/main/java/com/fd/algorithmlearn/Temp.java
+++ src/main/java/com/fd/algorithmlearn/Temp.java
@@ -842,6 +842,38 @@ public class Temp {
 //        }
 //    }
 
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+    /**
+     * 第一个错误的版本
+     * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+     * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+     * 你可以通过调用bool isBadVersion(version)接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+     * <p>
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnto1s/
+     * <p>
+     * 思路： 二分
+     * 第一个错误版本，在最后一个false的下一个
+     */
+    public int firstBadVersion(int n) {
+        int i = 1;
+        int j = n;
+        while (j > i) {
+            int mid = i + (j - i) / 2;
+            if (isBadVersion(mid)) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return i;
+    }
+
+    boolean isBadVersion(int version) {
+        return version >= 4;
+    }
+
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int[] nums2 = {2, 5, 6};
