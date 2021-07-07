ab9fc5065fbab604435617456551cc684adcfb59
zhangxinqiang <1933581361@qq.com>
2021-07-07 11:25:07 +0800

    leetcode 初级算法 买卖股票的最佳时机

--- src/main/java/com/fd/algorithmlearn/Temp.java
+++ src/main/java/com/fd/algorithmlearn/Temp.java
@@ -896,10 +896,41 @@ public class Temp {
         return dp[n];
     }
 
+    /**
+     * 买卖股票的最佳时机
+     * 给定一个数组 prices ，它的第i 个元素prices[i] 表示一支给定股票第 i 天的价格。
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     * <p>
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn8fsh/
+     *
+     * 思路：
+     * 1、当天能获得的最大利润 = 今天之前的股价最低的那天买入，今天卖出
+     * 2、记录之前的最低股价，记录最大利润
+     */
+    public static int maxProfit1(int[] prices) {
+        int min = prices[0];
+        int max = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - min;
+
+            if (profit > max) {
+                max = profit;
+            }
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
+        int[] x = {7, 1, 5, 3, 6, 4};
 
+        System.out.println(maxProfit1(x));
 
-        System.out.println(climbStairs(4));
+//        System.out.println(climbStairs(4));
 
 
 //        int[] nums1 = {1, 2, 3, 0, 0, 0};
