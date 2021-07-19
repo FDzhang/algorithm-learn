83bd0934368979122b9e7f66df488d1c5f8a4935
zhangxinqiang <1933581361@qq.com>
2021-07-19 11:00:28 +0800

    leetcode 初级算法 3的幂

--- src/main/java/com/fd/algorithmlearn/Temp.java
+++ src/main/java/com/fd/algorithmlearn/Temp.java
@@ -1080,10 +1080,44 @@ public class Temp {
         return ans;
     }
 
+    /**
+     * 3的幂
+     * 给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false 。
+     * <p>
+     * 整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3x
+     * <p>
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnsdi2/
+     */
+    public static boolean isPowerOfThree(int n) {
+        if (n < 1) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+
+    public static boolean isPowerOfThree1(int n) {
+        return (Math.log10(n) / Math.log10(3)) % 1 == 0;
+    }
+
+    public static boolean isPowerOfThree2(int n) {
+        return (n > 0 && 1162261467 % n == 0);
+    }
+
+
     public static void main(String[] args) {
         int[] x = {2, 7, 9, 3, 1};
-        System.out.println(countPrimes(499979));
 
+        System.out.println(isPowerOfThree(27));
+        System.out.println(isPowerOfThree(0));
+        System.out.println(isPowerOfThree(9));
+        System.out.println(isPowerOfThree(45));
+
+
+//        System.out.println(countPrimes(499979));
+//
 //        System.out.println(fizzBuzz(15));
 
 //        System.out.println(rob(x));
