0133d3f414f57fea7a6c915ea269e6b2b7d964a4
zhangxinqiang <1933581361@qq.com>
2021-07-20 11:33:42 +0800

    leetcode 初级算法题 罗马数字转整数

--- src/main/java/com/fd/algorithmlearn/Temp.java
+++ src/main/java/com/fd/algorithmlearn/Temp.java
@@ -1107,13 +1107,125 @@ public class Temp {
     }
 
 
+    /**
+     * 罗马数字转整数
+     * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+     * 字符          数值
+     * I             1
+     * V             5
+     * X             10
+     * L             50
+     * C             100
+     * D             500
+     * M             1000
+     * <p>
+     * 例如，罗马数字2 写做 II ，即为两个并列的1。12 写做 XII ，即为 X + II 。27 写做  XXVII,即为 XX + V + II 。
+     * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如4 不写做 IIII，而是 IV。数字1 在数字5 的左边，
+     * 所表示的数等于大数5 减小数1 得到的数值4 。同样地，数字9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+     * I 可以放在 V (5)和 X (10)的左边，来表示4 和9。
+     * X 可以放在 L (50)和 C (100)的左边，来表示40 和 90。 
+     * C 可以放在 D (500)和 M (1000)的左边，来表示 400 和 900。
+     * 给定一个罗马数字，将其转换成整数。输入确保在1 到3999 的范围内。
+     */
+    public static int romanToInt(String s) {
+        char[] chars = s.toCharArray();
+        HashMap<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        int sum = 0;
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (i == chars.length - 1) {
+                sum += roman.get(c);
+                continue;
+            }
+            char c1 = chars[i + 1];
+
+            if (c == 'I' && (c1 == 'V' || c1 == 'X')) {
+                sum = sum + roman.get(c1) - roman.get(c);
+                ++i;
+            } else if (c == 'X' && (c1 == 'L' || c1 == 'C')) {
+                sum = sum + roman.get(c1) - roman.get(c);
+                ++i;
+            } else if (c == 'C' && (c1 == 'D' || c1 == 'M')) {
+                sum = sum + roman.get(c1) - roman.get(c);
+                ++i;
+            } else {
+                sum = sum + roman.get(c);
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * MCMXCIV
+     * M = 1000, CM = 900, XC = 90, IV = 4.
+     *
+     * 1000 < 100 ?     res=1000
+     * 100  < 1000 ?    res = 900
+     * 1000 < 10 ?      res = 1900
+     * 10 < 100 ?       res = 1890
+     * 100 < 1 ？       res = 1990
+     * 1 < 5 ?         res = 1989
+     * 5               res = 1994
+     */
+    public int romanToInt1(String s) {
+        char[] chars = s.toCharArray();
+        int res = 0;
+
+        int preNum = getValue(chars[0]);
+        for (int i = 1; i < chars.length; i++) {
+            int curNum = getValue(chars[i]);
+            if (preNum < curNum) {
+                res -= preNum;
+            } else {
+                res += preNum;
+            }
+            preNum = curNum;
+        }
+        return res + preNum;
+    }
+
+    public int getValue(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
     public static void main(String[] args) {
         int[] x = {2, 7, 9, 3, 1};
 
-        System.out.println(isPowerOfThree(27));
-        System.out.println(isPowerOfThree(0));
-        System.out.println(isPowerOfThree(9));
-        System.out.println(isPowerOfThree(45));
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("IV"));
+        System.out.println(romanToInt("IX"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
+
+//        System.out.println(isPowerOfThree(27));
+//        System.out.println(isPowerOfThree(0));
+//        System.out.println(isPowerOfThree(9));
+//        System.out.println(isPowerOfThree(45));
 
 
 //        System.out.println(countPrimes(499979));
