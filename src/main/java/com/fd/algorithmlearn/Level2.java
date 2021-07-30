a551784c016371b23d91dcd9a722d7e35eded113
zhangxinqiang <1933581361@qq.com>
2021-07-30 14:12:12 +0800

    leetcode 中级算法 矩阵置零

--- src/main/java/com/fd/algorithmlearn/Level2.java
+++ src/main/java/com/fd/algorithmlearn/Level2.java
@@ -1,8 +1,6 @@
 package com.fd.algorithmlearn;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * https://leetcode-cn.com/leetbook/detail/top-interview-questions-medium/
@@ -32,7 +30,7 @@ public class Level2 {
      * 1、 排序
      * 2、 三个数字，保证第一个数字不重复。调用twoSum()
      * 3、 twoSum(nums, lo, target) : 返回两个值的list， 每个list中的两个值加起来=target (需要注意while循环放置的位置)
-     *
+     * <p>
      * 4、小优化，当nums[i]>0时，则没必要继续调用twoSum
      */
     public static List<List<Integer>> threeSum(int[] nums) {
@@ -87,5 +85,52 @@ public class Level2 {
             }
         }
         return res;
+    }
+
+    /**
+     * 矩阵置零
+     * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+     * 进阶：
+     * <p>
+     * 一个直观的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+     * 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+     * 你能想出一个仅使用常量空间的解决方案吗？
+     * <p>
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-medium/xvmy42/
+     * <p>
+     * 思路：
+     * 1、遍历矩阵第一遍标记，若 m[i][j] = 0, 则记录i、j
+     * 2、分别根据 记录的 i、j,将 i行置0,将 j列置0
+     * <p>
+     * 进阶思路： 仅使用常量空间
+     * 基于 O(m + n) 的额外空间 的方案： 无非需要 m+n 的额外空间 -> 借用矩阵的 第0行和第0列 作为m+n的额外空间
+     * 1、遍历第0行判断是否有0,记录一个标记a； 遍历第0列判断是否有0，记录一个标记b
+     * 2、从 (1,1) 开始遍历矩阵, 若 m(i,j)==0, 则将 i 记录到 m(i, 0), 将j 记录到 m(0, j)。将两个位置 置0
+     * 3、从 (1,1) 开始再次遍历矩阵, 遍历到 m(i,j)时, 若 （m(i, 0)==0 or m(0, j)==0）,则令m(i, j)=0;
+     * 4、根据标记a 判断是否需要将第0行置0， 根据标记b判断是否需要将第0列置0
+     */
+    public void setZeroes(int[][] matrix) {
+
+        Set<Integer> r = new HashSet<>();
+        Set<Integer> l = new HashSet<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    r.add(i);
+                    l.add(j);
+                }
+            }
+        }
+
+        for (Integer i : r) {
+            Arrays.fill(matrix[i], 0);
+        }
+
+        for (Integer j : l) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][j] = 0;
+            }
+        }
     }
 }
