7d099238394a019734f698b45af88b88ae4aea41
zhangxinqiang <1933581361@qq.com>
2021-09-09 16:05:32 +0800

    leetcode 中级算法题 搜索旋转排序数组

--- src/main/java/com/fd/algorithmlearn/Level2.java
+++ src/main/java/com/fd/algorithmlearn/Level2.java
@@ -27,6 +27,56 @@ public class Level2 {
     }
 
     /**
+     * 搜索旋转排序数组
+     * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+     * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+     * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+     * <p>
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-medium/xvyz1t/
+     * <p>
+     * nums = [4,5,6,7,0,1,2], target = 0
+     * <p>
+     * lo= 0  hi=6  mid=3
+     * nums[mid]>nums[right] -> target < nums[mid] && target < nums[left]  -> lo = mid+1 = 4
+     */
+    public int search(int[] nums, int target) {
+        int res = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public int search1(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < nums[hi]) {
+                if (target > nums[mid] && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            } else {
+                if (target < nums[mid] && target >= nums[lo]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    /**
      * 在排序数组中查找元素的第一个和最后一个位置
      * <p>
      * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
