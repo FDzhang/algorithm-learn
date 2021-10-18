750cf528c0c25b6cbdbeee94d824474e287e8d57
zhangxinqiang <1933581361@qq.com>
2021-10-18 16:10:31 +0800

    leetcode 高级算法题 盛最多水的容器

--- src/main/java/com/fd/algorithmlearn/Level3.java
+++ src/main/java/com/fd/algorithmlearn/Level3.java
@@ -16,6 +16,35 @@ import java.util.Map;
  */
 public class Level3 {
 
+
+    /**
+     * 盛最多水的容器
+     * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     * 说明：你不能倾斜容器。
+     * <p>
+     * 思路: 双指针
+     * 由于答案容器的面积与两个变量有关，我们从一个变量的最大值开始，
+     * 不断枚举所有可能(使得其中一个变量连续性的变小，而另一个变量可能变大也可能变小，但绝不是必定变小)
+     */
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+
+        int res = 0;
+        while (left < right) {
+            int m = Math.min(height[left], height[right]);
+            res = Math.max(res, m * (right - left));
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
+
+
     /**
      * 四数相加 II
      * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
