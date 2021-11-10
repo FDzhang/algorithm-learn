88a24e7b241d7d5d3aa16ca5f395304e8dd87961
zhangxinqiang <1933581361@qq.com>
2021-11-10 16:42:53 +0800

    leetcode 高级算法题 课程表 II

--- src/main/java/com/fd/algorithmlearn/Level3.java
+++ src/main/java/com/fd/algorithmlearn/Level3.java
@@ -18,6 +18,62 @@ public class Level3 {
     // -------------------------- 树和图 ------------------------------
 
     /**
+     * 课程表 II
+     * 现在你总共有 numCourses 门课需要选，记为 0 到 numCourses - 1。给你一个数组 prerequisites ，
+     * 其中 prerequisites[i] = [ai, bi] ，表示在选修课程 ai 前 必须 先选修 bi 。
+     * <p>
+     * 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示：[0,1] 。
+     * 返回你为了学完所有课程所安排的学习顺序。可能会有多个正确的顺序，你只要返回 任意一种 就可以了。如果不可能完成所有课程，返回 一个空数组 。
+     * <p>
+     * 思路：拓扑排序
+     * 1、依据 numCourses 和 prerequisites[][], 构建一个图
+     * 2、使用dfs遍历图, 借助visited[]和onPath[]判断是否有环
+     * - a、onPath[]： 记录一次 dfs 递归经过的节点
+     * - b、visited[]： 记录遍历过的节点，防止走回头路
+     * 3、若有环则直接返回一个空数组
+     * 4、借助 postOrder 记录后序遍历的结果, 返回 翻转后的postOrder (即为拓扑排序的结果)
+     */
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graph = buildGraph(numCourses, prerequisites);
+
+        boolean[] visited = new boolean[numCourses];
+        // onPath[0] 存检测结果
+        boolean[] onPath = new boolean[numCourses + 1];
+        List<Integer> postOrder = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            traverse(graph, i, visited, onPath, postOrder);
+        }
+
+        if (onPath[0]) {
+            return new int[]{};
+        }
+
+        Collections.reverse(postOrder);
+        // 类型转换 List 转 Array
+        return postOrder.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
+    private void traverse(List<Integer>[] graph, int s, boolean[] visited, boolean[] onPath, List<Integer> postOrder) {
+        if (onPath[s + 1]) {
+            onPath[0] = true;
+        }
+        if (visited[s] || onPath[0]) {
+            return;
+        }
+        visited[s] = true;
+
+        onPath[s + 1] = true;
+        for (int i : graph[s]) {
+            traverse(graph, i, visited, onPath, postOrder);
+        }
+        onPath[s + 1] = false;
+
+        // 后序遍历的位置
+        postOrder.add(s);
+    }
+
+    /**
      * 课程表
      * 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
      * <p>
