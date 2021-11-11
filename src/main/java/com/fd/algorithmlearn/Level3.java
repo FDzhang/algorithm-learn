bbf73e29c4cbb0d4ff82f11c87c079d2cfc94ecb
zhangxinqiang <1933581361@qq.com>
2021-11-11 20:01:11 +0800

    leetcode 高级算法题 矩阵中的最长递增路径 todo

--- src/main/java/com/fd/algorithmlearn/Level3.java
+++ src/main/java/com/fd/algorithmlearn/Level3.java
@@ -18,6 +18,20 @@ public class Level3 {
     // -------------------------- 树和图 ------------------------------
 
     /**
+     * 矩阵中的最长递增路径
+     * 给定一个 m x n 整数矩阵 matrix ，找出其中 最长递增路径 的长度。
+     * 对于每个单元格，你可以往上，下，左，右四个方向移动。 你 不能 在 对角线 方向上移动或移动到 边界外（即不允许环绕）。
+     *
+     * 思路:
+     * 思路：dfs
+     * 1、遍历矩阵, 对每个节点进行dfs
+     *
+     */
+    public int longestIncreasingPath(int[][] matrix) {
+        return 0;
+    }
+
+    /**
      * 课程表 II
      * 现在你总共有 numCourses 门课需要选，记为 0 到 numCourses - 1。给你一个数组 prerequisites ，
      * 其中 prerequisites[i] = [ai, bi] ，表示在选修课程 ai 前 必须 先选修 bi 。
