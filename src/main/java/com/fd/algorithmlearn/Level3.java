2588aacbc4c156bf867dc2b973bbe60c34403d59
zhangxinqiang <1933581361@qq.com>
2021-12-04 11:34:04 +0800

    leetcode 高级算法题 最佳买卖股票时机含冷冻期

--- src/main/java/com/fd/algorithmlearn/Level3.java
+++ src/main/java/com/fd/algorithmlearn/Level3.java
@@ -21,25 +21,63 @@ public class Level3 {
     /**
      * 最佳买卖股票时机含冷冻期
      * 给定一个整数数组，其中第 i 个元素代表了第 i 天的股票价格 。​
-     *
+     * <p>
      * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
-     *
+     * <p>
      * 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
      * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
-     *
+     * <p>
      * 1,2,3,0,2
      * 1  0
      * 2  1
      * 3  2
      * 0  2
      * 2  3
-     *
+     * <p>
      * 思路：动态规划
-     * 1、dp[i]: 到第i天能获得的最大利润
-     * 2、
+     * 1、由题，设定三种状态
+     * a、dp[i][0]  第i天结束时，持有股票的最大收益
+     * b、dp[i][1]  第i天结束时，无股票 且处于冷冻期 的最大收益
+     * c、dp[i][2]  第i天结束时，无股票 且不处于冷冻期 的最大收益
+     * 2、针对三种状态的转移方程
+     * a、dp[i][0] =  max(dp[i-1][0], dp[i-1][2]-price[i]) （昨天就已经持有 or 前天卖，今天买 ）
+     * b、dp[i][1] =  dp[i-1][0]+price[i] （今天卖）
+     * c、dp[i][2] =  max(dp[i-1][1], dp[i-1][2]) （昨天卖，今无操作）
+     * <p>
+     * 空间优化：
      */
     public int maxProfit(int[] prices) {
-        return 0;
+        int len = prices.length;
+        if (len == 0) {
+            return 0;
+        }
+
+        int buy = -prices[0];
+        int frozen = 0;
+        int noFrozen = 0;
+        for (int i = 1; i < len; i++) {
+            buy = Math.max(buy, noFrozen - prices[i]);
+            noFrozen = Math.max(frozen, noFrozen);
+            frozen = buy + prices[i];
+        }
+
+        return Math.max(frozen, noFrozen);
+    }
+
+    public int maxProfit1(int[] prices) {
+        int len = prices.length;
+        if (len == 0) {
+            return 0;
+        }
+        int[][] dp = new int[len][3];
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            dp[i][1] = dp[i - 1][0] + prices[i];
+            dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][2]);
+        }
+
+        return Math.max(dp[len - 1][1], dp[len - 1][2]);
     }
 
     /**
