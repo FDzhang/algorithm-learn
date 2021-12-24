95d3de4f9bcc5c480ede995c6870cfaccddbe2a2
zhangxinqiang <1933581361@qq.com>
2021-12-24 16:39:03 +0800

    leetcode 高级算法题 直线上最多的点数 方法2

--- src/main/java/com/fd/algorithmlearn/Level3.java
+++ src/main/java/com/fd/algorithmlearn/Level3.java
@@ -58,6 +58,29 @@ public class Level3 {
     public int gcd(int a, int b) {
         return b == 0 ? a : gcd(b, a % b);
     }
+    // 暴力
+    public int maxPoints1(int[][] ps) {
+        int n = ps.length;
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            int[] x = ps[i];
+            for (int j = i + 1; j < n; j++) {
+                int[] y = ps[j];
+                int cnt = 2;
+                for (int k = j + 1; k < n; k++) {
+                    int[] p = ps[k];
+                    int s1 = (y[1] - x[1]) * (p[0] - y[0]); // 原理 a/b=c/d  -->  ad=bc
+                    int s2 = (p[1] - y[1]) * (y[0] - x[0]);
+                    if (s1 == s2){
+                        cnt++;
+                    }
+                }
+                ans = Math.max(ans, cnt);
+            }
+        }
+        return ans;
+    }
+
 
     /**
      * 最大数
