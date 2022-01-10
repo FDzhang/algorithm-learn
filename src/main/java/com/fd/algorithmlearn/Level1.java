eeebdd860925770499c441cda356e3c119ef201c
zhangxinqiang <1933581361@qq.com>
2022-01-10 13:12:06 +0800

    leetcode 初级算法题整理 旋转图像

--- src/main/java/com/fd/algorithmlearn/Level1.java
+++ src/main/java/com/fd/algorithmlearn/Level1.java
@@ -73,6 +73,20 @@ public class Level1 {
             }
         }
     }
+    public void rotate1(int[][] matrix) {
+        int length = matrix.length;
+        // 因为是对称的，只需要计算循环前半行即可
+        for (int i = 0; i < length / 2; i++) {
+            for (int j = i; j < length - i - 1; j++) {
+                int temp = matrix[i][j];
+                int m = length - j - 1;
+                int n = length - i - 1;
+                matrix[i][j] = matrix[m][i];
+                matrix[m][i] = matrix[n][m];
+                matrix[n][m] = matrix[j][n];
+                matrix[j][n] = temp;
+            }
+    }
 
     /**
      * 有效的数独
