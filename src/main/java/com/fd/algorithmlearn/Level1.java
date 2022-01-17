286cef76549cd9d90866c3257d9e07190c4b3f8a
zhangxinqiang <1933581361@qq.com>
2022-01-17 10:38:00 +0800

    leetcode 初级算法题整理 外观数列

--- src/main/java/com/fd/algorithmlearn/Level1.java
+++ src/main/java/com/fd/algorithmlearn/Level1.java
@@ -22,6 +22,64 @@ public class Level1 {
     // 我们推荐以下题目：反转字符串，字符串中第一个唯一字符，字符串转整数（atoi）和 实现 strStr() 。
 
     /**
+     * 外观数列
+     * 给定一个正整数 n ，输出外观数列的第 n 项。
+     *
+     * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+     *
+     * 你可以将其视作是由递归公式定义的数字字符串序列：
+     *
+     * countAndSay(1) = "1"
+     * countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+     * 前五项如下：
+     *
+     * 1.     1
+     * 2.     11
+     * 3.     21
+     * 4.     1211
+     * 5.     111221
+     * 第一项是数字 1
+     * 描述前一项，这个数是 1 即 “ 一 个 1 ”，记作 "11"
+     * 描述前一项，这个数是 11 即 “ 二 个 1 ” ，记作 "21"
+     * 描述前一项，这个数是 21 即 “ 一 个 2 + 一 个 1 ” ，记作 "1211"
+     * 描述前一项，这个数是 1211 即 “ 一 个 1 + 一 个 2 + 二 个 1 ” ，记作 "111221"
+     * 要 描述 一个数字字符串，首先要将字符串分割为 最小 数量的组，每个组都由连续的最多 相同字符 组成。然后对于每个组，
+     * 先描述字符的数量，然后描述字符，形成一个描述组。要将描述转换为数字字符串，先将每组中的字符数量用数字替换，再将所有描述组连接起来。
+     *
+     * 思路：
+     * 1、初始化外观数列的第一项 "1"
+     * 2、以第一项为基础 循环处理外观数列n次,规则如题所述
+     * a、遍历外观字符串s, 记录连续字符的次数cnt和字符本身c，拼接cnt、c，到结果集
+     *
+     */
+    public String countAndSay(int n) {
+        String str = "1";
+        if (n == 1) {
+            return str;
+        }
+        for (int i = 1; i < n; i++) {
+            str = say(str);
+        }
+        return str;
+    }
+
+    private String say(String s) {
+        char[] cs = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        int count = 1;
+        for (int i = 1; i < cs.length; i++) {
+            if (cs[i - 1] != cs[i]) {
+                sb.append(count).append(cs[i - 1]);
+                count = 0;
+            }
+            count++;
+        }
+        sb.append(count).append(cs[cs.length - 1]);
+        return sb.toString();
+    }
+
+    /**
      * 实现 strStr()
      * 实现 strStr() 函数。
      * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。
