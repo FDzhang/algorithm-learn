8ebf08a67392674bb67475b18326067794bc98c3
zhangxinqiang <1933581361@qq.com>
2022-02-11 01:41:07 +0800

    leetcode 初级算法题整理(排序和搜索) 打家劫舍

--- src/main/java/com/fd/algorithmlearn/Level1.java
+++ src/main/java/com/fd/algorithmlearn/Level1.java
@@ -25,6 +25,87 @@ public class Level1 {
     // 我们推荐以下题目：爬楼梯，买卖股票最佳时机 和 最大子序和。
 
     /**
+     * 打家劫舍
+     * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
+     * 影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     * <p>
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+     * <p>
+     * 相关标签 数组 动态规划
+     * <p>
+     * 思路1：动态规划 原始
+     * 1、定义数组dp[n][2]
+     * a、dp[i][0]:偷i房，[0,i]房所能窃到的最高金额 max(dp[i-2][0],dp[i-2][1])+nums[i]
+     * b、dp[i][1]:不偷i房，[0,i]房所能窃到的最高金额 max(dp[i-2][0],dp[i-1][1])
+     * 2、初始化：
+     * a、dp[0][0]=nums[0], dp[0][1]=0
+     * b、dp[1][0]=nums[1], dp[1][1]=nums[0]
+     * 3、res=max(dp[n][0], dp[n][1])
+     * <p>
+     * 思路2：动态规划
+     * 1、定义数组dp[i]: [0,i]房所能窃到的最高金额
+     * 2、dp[0]=nums[0], dp[1]=max(nums[0], nums[1]), dp[i]=max(dp[n-1], dp[n-2]+nums[i])
+     * ps: 空间优化：只依赖 dp[i]只依赖 dp[i-1],dp[i-2], 所以使用3个常量即可
+     * <p>
+     * 思路3：递归
+     * 1、
+     */
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0 || n == 1) {
+            return n == 0 ? 0 : nums[0];
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+
+    // 空间优化
+    public int rob2(int[] nums) {
+        int n = nums.length;
+        if (n == 0 || n == 1) {
+            return n == 0 ? 0 : nums[0];
+        }
+        int p = nums[0];
+        int q = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            int tmp = Math.max(q, p + nums[i]);
+            p = q;
+            q = tmp;
+        }
+        return q;
+    }
+
+    public int rob1(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int n = nums.length;
+
+        int[][] dp = new int[n][2];
+        dp[0][0] = nums[0];
+        dp[0][1] = 0;
+        dp[1][0] = nums[1];
+        dp[1][1] = nums[0];
+        for (int i = 2; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 2][0], dp[i - 2][1]) + nums[i];
+            dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+        }
+        return Math.max(dp[n - 1][0], dp[n - 1][1]);
+    }
+
+
+    /**
      * 最大子序和
      * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
      * <p>
