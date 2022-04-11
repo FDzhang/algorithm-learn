68306a07a22928afeab907a736ae05dad6a325e5
zhangxinqiang <1933581361@qq.com>
2022-04-11 13:35:29 +0800

    leetcode 中级算法题整理 搜索旋转排序数组

--- src/main/java/com/fd/algorithmlearn/Level22.java
+++ src/main/java/com/fd/algorithmlearn/Level22.java
@@ -27,6 +27,85 @@ public class Level22 {
     //
 
     /**
+     * 搜索旋转排序数组
+     * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+     * <p>
+     * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ...,
+     * nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+     * <p>
+     * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * 输入：nums = [4,5,6,7,0,1,2], target = 0
+     * 输出：4
+     * 示例 2：
+     * <p>
+     * 输入：nums = [4,5,6,7,0,1,2], target = 3
+     * 输出：-1
+     * 示例 3：
+     * <p>
+     * 输入：nums = [1], target = 0
+     * 输出：-1
+     *  
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= nums.length <= 5000
+     * -10^4 <= nums[i] <= 10^4
+     * nums 中的每个值都 独一无二
+     * 题目数据保证 nums 在预先未知的某个下标上进行了旋转
+     * -10^4 <= target <= 10^4
+     * <p>
+     * 进阶：你可以设计一个时间复杂度为 O(log n) 的解决方案吗？
+     * <p>
+     * 相关标签 数组 二分查找
+     * <p>
+     * 思路1：
+     * 1、遍历nums查找target
+     * <p>
+     * 思路2：二分 （from讨论区热评top1  二分查找的本质——根据有序元素段判断元素位置以不断收缩边界选择搜索空间）
+     * 1、依据 局部有序，不断收缩边界
+     * 2、nums[mid]>=nums[i], 则左端有序。
+     * a、target < nums[mid] 且 target >= nums[i] 则元素在左端，令j=mid-1，反之，令i=mid+1;
+     * 3、nums[mid] < nums[i], 则右端有序。
+     * a、target > nums[mid] 且 target <= nums[j] 则元素在右端，令i=mid+1，反之，令j=mid-1;
+     */
+    public int search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[i]) {
+                if (target < nums[mid] && target >= nums[i]) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            } else if (nums[mid] < nums[i]) {
+                if (target > nums[mid] && target <= nums[j]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int search1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
      * 合并区间
      * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
      * 请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
