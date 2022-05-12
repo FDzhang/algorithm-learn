8ca8ddac8b5649a3200b216c348736d022ae398f
zhangxinqiang <1933581361@qq.com>
2022-05-12 10:41:58 +0800

    leetcode 高级算法题整理 四数相加 II

--- src/main/java/com/fd/algorithmlearn/Level33.java
+++ src/main/java/com/fd/algorithmlearn/Level33.java
@@ -2,6 +2,7 @@ package com.fd.algorithmlearn;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -24,6 +25,57 @@ public class Level33 {
     // 后面两道题的难度会稍有增加，如果你解题遇到困难，请随时跳过这些题目，你可以留到之后再回来思考。
     //
     // 除自身以外数组的乘积
+
+    /**
+     * 四数相加 II
+     * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+     * <p>
+     * 0 <= i, j, k, l < n
+     * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+     * 示例 1：
+     * 输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+     * 输出：2
+     * 解释：
+     * 两个元组如下：
+     * 1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+     * 2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+     * 示例 2：
+     * <p>
+     * 输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+     * 输出：1
+     *  
+     * <p>
+     *   提示：
+     * n == nums1.length
+     * n == nums2.length
+     * n == nums3.length
+     * n == nums4.length
+     * 1 <= n <= 200
+     * -228 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 228
+     * 相关标签 数组 哈希表
+     * <p>
+     * 思路1：哈希表
+     * 1、初始化map：key数字，value数字出现的次数
+     * 2、遍历nums1，nums2 将两者相加的结果作为map的key，value为出现的次数
+     * 3、遍历nums3，nums4，若两者相加的结果再乘以-1，在map中出现过，则res加上此数出现的次数
+     */
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            for (int j : nums2) {
+                map.merge(i + j, 1, Integer::sum);
+            }
+        }
+
+        int res = 0;
+        for (int i : nums3) {
+            for (int j : nums4) {
+                int t = -(i + j);
+                res += map.getOrDefault(t, 0);
+            }
+        }
+        return res;
+    }
 
     /**
      * 螺旋矩阵
