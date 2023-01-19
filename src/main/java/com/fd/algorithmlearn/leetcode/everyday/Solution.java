34f44e8a0c627a0616a8d0b468b0722659225967
zhangxinqiang <1933581361@qq.com>
2023-01-19 20:38:56 +0800

    FreqStack

--- src/main/java/com/fd/algorithmlearn/leetcode/everyday/Solution.java
+++ src/main/java/com/fd/algorithmlearn/leetcode/everyday/Solution.java
@@ -3,6 +3,140 @@ package com.fd.algorithmlearn.leetcode.everyday;
 import java.util.*;
 
 public class Solution {
+    static class FreqStack {
+        Map<Integer, Integer> map;
+        PriorityQueue<int[]> stack;
+
+        int op;
+
+        public FreqStack() {
+            op = 0;
+            map = new HashMap<>();
+            stack = new PriorityQueue<>((a, b) -> b[1] == a[1] ? Integer.compare(b[2], a[2]) : Integer.compare(b[1], a[1]));
+        }
+
+        public void push(int val) {
+            if (!map.containsKey(val)) {
+                map.put(val, 0);
+            }
+            map.put(val, map.get(val) + 1);
+            stack.add(new int[]{val, map.get(val), ++op});
+        }
+
+        public int pop() {
+            int[] top = stack.poll();
+            map.put(top[0], map.get(top[0]) - 1);
+            return top[0];
+        }
+
+        public static void main(String[] args) {
+            FreqStack freqStack = new FreqStack();
+            freqStack.push(5);
+            freqStack.push(7);
+            freqStack.push(5);
+            freqStack.push(7);
+            freqStack.push(4);
+            freqStack.push(5);
+            System.err.println(freqStack.pop());
+            System.err.println(freqStack.pop());
+            System.err.println(freqStack.pop());
+            System.err.println(freqStack.pop());
+        }
+    }
+
+    /**
+     * 1、路径：已经走过的路径
+     * 2、选择列表：可以连接的节点。
+     * 3、结束条件：达到长度n
+     */
+    class Node {
+        public int max = 0;
+        public boolean arrived = false;
+    }
+
+    public int reachableNodes(int[][] edges, int maxMoves, int n) {
+        // 连接关系
+        Map<String, int[]> res = new HashMap<>();
+        Map<Integer, List<Integer>> select = new HashMap<>();
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            int cnt = edge[2];
+
+            res.put(a + "-" + b, new int[]{0, cnt});
+            res.put(b + "-" + a, new int[]{0, cnt});
+            if (!select.containsKey(a)) {
+                select.put(a, new ArrayList<>());
+            }
+            if (!select.containsKey(b)) {
+                select.put(b, new ArrayList<>());
+            }
+            select.get(a).add(b);
+            select.get(b).add(a);
+        }
+        Set<Integer> path = new LinkedHashSet<>();
+        Node[] nodeCnt = new Node[n];
+        for (int i = 0; i < nodeCnt.length; i++) {
+            nodeCnt[i] = new Node();
+        }
+        nodeCnt[0].arrived = true;
+        nodeCnt[0].max = maxMoves;
+        path.add(0);
+        reachable(0, path, select, maxMoves, res, nodeCnt);
+
+        int ans = 0;
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            int cnt = edge[2];
+
+            String ab = a + "-" + b;
+            String ba = b + "-" + a;
+            ans += Math.min(cnt, res.get(ab)[0] + res.get(ba)[0]);
+
+            if (res.get(ab)[0] + res.get(ba)[0] < cnt) {
+                System.err.println(ab + Arrays.toString(res.get(ab)) + Arrays.toString(res.get(ba)));
+            }
+        }
+        int nCnt = 0;
+        for (Node b : nodeCnt) {
+            if (b.arrived) {
+                nCnt++;
+            }
+        }
+        return ans + nCnt;
+    }
+
+    private void reachable(int cur, Set<Integer> path, Map<Integer, List<Integer>> select, int maxMoves,
+                           Map<String, int[]> res, Node[] nodeCnt) {
+        System.err.println(path + ", " + maxMoves);
+        if (maxMoves < 0) {
+            return;
+        }
+        nodeCnt[cur].arrived = true;
+        if (cur != 0 && maxMoves <= nodeCnt[cur].max) {
+            return;
+        }
+        nodeCnt[cur].max = maxMoves;
+
+        if (maxMoves == 0) {
+            return;
+        }
+        if (select.get(cur) == null) {
+            return;
+        }
+        for (Integer next : select.get(cur)) {
+            String key = cur + "-" + next;
+            res.get(key)[0] = Math.min(Math.max(maxMoves, res.get(key)[0]), res.get(key)[1]);
+            if (path.contains(next)) {
+                continue;
+            }
+            path.add(next);
+            reachable(next, path, select, maxMoves - 1 - res.get(key)[1], res, nodeCnt);
+            path.remove(next);
+        }
+    }
+
 
     /**
      * 100
@@ -187,9 +321,19 @@ public class Solution {
 //        String[] ss = {"a", "bb", "acd", "ace"};
 //
         Solution solution = new Solution();
+        int[][] edges = new int[][]{{0, 1, 10}, {0, 2, 1}, {1, 2, 2}};
+//        int[][] edges = new int[][]{{0, 1, 4}, {1, 2, 6}, {0, 2, 8}, {1, 3, 1}};
+
+//        int[][] edges = new int[][]{{4, 6, 11}, {5, 6, 2}, {2, 6, 11}, {0, 3, 19},
+//                {1, 7, 21}, {5, 7, 1}, {1, 5, 4}, {0, 7, 12}, {6, 7, 3}, {3, 6, 22},
+//                {0, 5, 24}, {1, 2, 8}, {3, 7, 11},
+//                {1, 3, 14}, {4, 5, 7}, {4, 7, 14}, {0, 4, 5}, {2, 4, 7}, {3, 4, 11},
+//                {3, 5, 15}, {2, 5, 13}, {2, 3, 6}, {1, 4, 6}, {0, 2, 3}, {1, 6, 20}};
+        System.err.println(solution.reachableNodes(edges, 6, 3));
+
 //        int cnt = solution.numMatchingSubseq(s, ss);
 //        System.err.println(cnt);
-        System.err.println(solution.soupServings(200 * 25));
+//        System.err.println(solution.soupServings(200 * 25));
 
 //        System.err.println(solution.toHex(15));
 //        System.err.println(solution.toHex(16));
